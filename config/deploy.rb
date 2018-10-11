2bef4abde18471df10d9de399346424f7a486e48
Beastie PC <gskondratenko@opora.org.ua>
2018-10-12 01:54:00 +0300

    Addede setings config

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :port, '1122'           # SSH port number.
 
 # shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
 set :shared_dirs, fetch(:shared_dirs, []).push('log', 'public/image')
-set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
+set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml', 'config/settings.yml')
 invoke :'rvm:use', 'ruby-2.4.1@default'
 # This task is the environment that is loaded for all remote run commands, such as
 # `mina deploy` or `mina rake`.
@@ -43,6 +43,7 @@ task :setup do
   # command %{rbenv install 2.3.0}
   command %[touch "#{fetch(:shared_path)}/config/database.yml"]
   command %[touch "#{fetch(:shared_path)}/config/secrets.yml"]
+  command %[touch "#{fetch(:shared_path)}/"]
   command %[echo "-----> Be sure to edit '#{fetch(:deploy_to)}/#{fetch(:shared_path)}/config/database.yml' and 'secrets.yml'."]
 end
 
